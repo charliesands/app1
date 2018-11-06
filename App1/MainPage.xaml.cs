149f2a67b23708972a29e19bed8eb84d69aadbd8
charliesands <cwsands3@gmail.com>
2018-11-06 15:08:11 -0700

    pool puzzle exercise

--- App1/MainPage.xaml.cs
+++ App1/MainPage.xaml.cs
@@ -54,5 +54,36 @@ namespace App1
 
             output.Text = result;
         }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            int x = 0;
+            string poem = "";
+
+            while (x < 4)
+            {
+                poem = poem + "a";
+            if (x < 1)
+            {
+                poem = poem + " ";
+            }
+            poem = poem + "n";
+            if (x > 1)
+            {
+                poem = poem + " oyster";
+                x = x + 2;
+            }
+            if (x == 1)
+            {
+                poem = poem + "noys ";
+            }
+            if (x < 1)
+            {
+                poem = poem + "oise ";
+            }
+            x = x + 1;
+            }
+        }
+        output.Text = poem;
     }
 }
